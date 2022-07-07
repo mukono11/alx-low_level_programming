29f26e586cbdc4a4fab813a25090144676293067
mukono11 <mosesmahaja@gmail.com>
2022-07-07 08:12:38 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+ * main-Entry Point
+ *
+ * Description - Print all numbers 0 to 9
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
